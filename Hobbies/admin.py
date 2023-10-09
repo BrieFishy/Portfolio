dd8d84b8906540a7758fdc54018cd3b13e75bbd7
BrieFishy <112446655+BrieFishyW@users.noreply.github.com>
2023-10-08 21:00:24 -0600

    Combined all under one app

--- Hobbies/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Hobby
-
-# Register your models here.
-admin.site.register(Hobby)
-
--- Home/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-# Create your views here.
-def index(request):
-    return HttpResponse(
-        'Hello! My name is Briella, welcome to my page! My pronouns are they/them and I like paleontology.')
-
