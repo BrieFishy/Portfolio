d1a31f77c11abce09412eeb7509fa608a525a5f9
BrieFishy <112446655+BrieFishyW@users.noreply.github.com>
2023-10-09 22:52:14 -0600

    finished

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -4,6 +4,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', views.portfolio, name="portfolio"),
+    path('<int:item_id>', views.project_detail, name="project_detail"),
+    path('hobbies/<int:item_id>', views.hobby_detail, name="hobby_detail"),
     path('home/', views.home, name="home"),
     path('hobbies/', views.hobbies, name="hobbies"),
     path('contact/', views.contact, name="contact"),
--- Portfolio/views.py
+++ Portfolio/views.py
@@ -1,25 +1,49 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Project
-from .models import Hobby
+from .models import Project, Hobby
+from django.template import loader
 
 
 # Create your views here.
 def portfolio(request):
     project_list = Project.objects.all()
-    return HttpResponse(project_list)
+    context = {
+        'project_list': project_list,
+    }
+    return render(request, 'portfolio/index.html', context)
+
+
+def project_detail(request, item_id):
+    item = Project.objects.get(pk=item_id)
+    context = {
+        'name': item.project_name,
+        'description': item.project_description,
+        'image': item.project_image,
+    }
+    return render(request, 'portfolio/detail.html', context)
 
 
 def contact(request):
-    return HttpResponse(
-        'jrutherford@mail.weber.edu')
+    return render(request, 'portfolio/contact.html', {})
 
 
 def hobbies(request):
     hobby_list = Hobby.objects.all()
-    return HttpResponse(hobby_list)
+    context = {
+        'hobby_list': hobby_list,
+    }
+    return render(request, 'portfolio/hobbies.html', context)
+
+
+def hobby_detail(request, item_id):
+    item = Hobby.objects.get(pk=item_id)
+    context = {
+        'name': item.hobby_name,
+        'description': item.hobby_description,
+        'image': item.hobby_image,
+    }
+    return render(request, 'portfolio/detail.html', context)
 
 
 def home(request):
-    return HttpResponse(
-        'Hello! My name is Briella, welcome to my page! My pronouns are they/them and I like paleontology.')
+    return render(request, 'portfolio/home.html', {})
